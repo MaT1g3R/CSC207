c2a7dd250c19def85eb31b17c0d7537ae7475108
Andrew Olechtchouk <Andrew@notawifinetwork.local>
2017-03-10 02:50:08 -0500

    Updated UML diagram and modified the abstract Worker.java class respectively.

--- project/src/warehouse/Worker.java
+++ project/src/warehouse/Worker.java
@@ -12,30 +12,38 @@ import java.util.ArrayList;
 public abstract class Worker {
 
   private String name;
-  private ArrayList<Integer> currentFascias;
+  private ArrayList<Fascia> currentFascias;
   private String logFilePath;
   
-  protected Worker (String name, String logFilePath){
+  protected Worker (String name, String logFilePath, ArrayList<Fascia> currentFascias){
     this.name = name;
     this.logFilePath = logFilePath;
+    this.currentFascias = currentFascias;
   }
 
   /**
-   * @return the name
+   * @return the name of the worker
    */
   public String getName() {
     return name;
   }
 
   /**
-   * @param name the name to set
+   * @param name the name to change to if the worker decides to change their name
    */
-  public void setName(String name) {
+  public void changeName(String name) {
     this.name = name;
   }
   
-  protected abstract void scan();
+  /**
+   * @return the currentFascias
+   */
+  public ArrayList<Fascia> getCurrentFascias() {
+    return currentFascias;
+  }
+
   protected abstract void logEvent();
+  protected abstract void performJob();
   
   
 }
