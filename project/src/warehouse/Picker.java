d687ddd8b9ac65b49573aca7df380b4c882111c4
Tasbir Rahman <Tasbir Rahman>
2017-03-11 02:36:09 -0500

    Created Picker Class - Tasbir Rahman

---
+++ project/src/warehouse/Picker.java
@@ -0,0 +1,28 @@
+package warehouse;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by Tasbir on 2017-03-11.
+ */
+public class Picker extends Worker {
+
+  public Picker(String name, Warehouse worksAt) {
+    super(name, worksAt);
+  }
+
+  @Override
+  public LinkedList<Integer> getScanOrder(PickingRequest currPickReq) {
+    ArrayList<String> loc = WarehousePicking.optimize(currPickingReq.getSkus());
+    LinkedList<Integer> output = new LinkedList<>();
+    for(String x:loc) {
+
+      output.add(SkuTranslator.getSkuFromLocation(x.split(",")));
+    }
+    return output;
+
+  }
+
+
+}
