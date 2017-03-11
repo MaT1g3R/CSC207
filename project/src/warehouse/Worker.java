f24a9408b3926b8385e6dea4c6d820fcde1ff26d
Andrew Olechtchouk <Andrew@user64-71.vicres.utoronto.ca>
2017-03-11 16:10:07 -0500

    Modified Worker.java to include new methods for handling the workers.

--- project/src/warehouse/Worker.java
+++ project/src/warehouse/Worker.java
@@ -16,15 +16,15 @@ public abstract class Worker {
   protected Warehouse worksAt;
   private boolean isReady = true;
   protected PickingRequest currPickingReq;
+  private String role;
 
   protected Worker(String name, Warehouse worksAt) {
     this.name = name;
     this.worksAt = worksAt;
-    this.toBeScanned = getScanOrder(currPickingReq);
-
+    this.role = String.class.getSimpleName();
   }
 
-  protected abstract LinkedList<Integer> getScanOrder(PickingRequest currPick);
+  protected abstract LinkedList<Integer> getScanOrder();
 
   /**
    * @return the name of the worker
@@ -38,11 +38,33 @@ public abstract class Worker {
    * @param sku the SKU to be scanned.
    */
   public void scan(int sku) {
-    if (sku == toBeScanned.pop()) {
-      System.out.println("Scan successful - correct SKU selected.");
-    } else {
-      System.out.println("ERROR: Scan unsuccessful - incorrect SKU selected.");
+    System.out.println(role + " " + name + " Scanned " + sku );
+    if (!toBeScanned.isEmpty()) {
+      if (sku != toBeScanned.pop()){
+        System.out.println(role + " " + name + " Wrong Scan");
+        wrongScanHandle();
+      }
+      else{
+        System.out.println(role + " " + name + " Correct Scan");
+      }
+    } 
+    else{
+      System.out.println(role + " " + name + " Unneeded Scan!");
     }
+  }
+  
+  public void wrongScanHandle(){
+    //default behaviour is wrongScan = new picking request.
+    //REPLENEISHER MUST OVERRIDE THIS METHOD SINCE THEY DONT HANDLE PICK REQUESTS
+    System.out.println("Aborting action of " + role + " " + name);
+    worksAt.pickingRequests.add();
+    finish();
+  }
+  
+  public void finish(){
+    System.out.println("Current action of " + role + " " + name + " has finished.");
+    toBeScanned = new LinkedList<>();
+    this.isReady = true;
   }
 
 
@@ -59,9 +81,13 @@ public abstract class Worker {
   public void setIsReady(boolean isReady) {
     this.isReady = isReady;
   }
-
-  public void setCurrPickingReq(PickingRequest currPickingReq) {
+ 
+  public void start (PickingRequest currPickingReq){
+    
+    this.isReady = false;
     this.currPickingReq = currPickingReq;
+    this.toBeScanned = getScanOrder();
+    
   }
 
 
