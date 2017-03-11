d646f84b068e04ade88efce08ca13c2e1ca7c5a2
Tasbir Rahman <Tasbir Rahman>
2017-03-11 00:31:56 -0500

    Finished SkuTranslator documentation

--- project/src/warehouse/SkuTranslator.java
+++ project/src/warehouse/SkuTranslator.java
@@ -2,22 +2,28 @@ package warehouse;
 
 import java.util.ArrayList;
 
+/**
+ *  This class is responsible for taking in csv files that have data about SKUs and returning
+ *  information for a particular set of that data, or about an SKU
+ *
+ */
+
 public class SkuTranslator {
 
 
   private SkuTranslator() {
   }
-
+  
   private static ArrayList<ArrayList<String>> locations;
   private static ArrayList<ArrayList<String>> properties;
 
 
   /**
-   * Using the given translation table, this function takes in a sku as an int and returns its
-   * properties like Model, Colour, and whether or not it's a front or back fascia.
+   * Using the given translation table, this function takes in a <sku></sku> as an int and returns
+   * its properties like Model, Colour, and whether or not it's a front or back fascia.
    *
    * @param sku The SKU who's property is being looked for.
-   * @return String array representing properties. The format is [model, colour, direction]
+   * @return String array representing properties of sku. The format is [model, colour, direction]
    */
   public static String[] getProperties(int sku) {
     String[] output = {"None","None", "None"};
@@ -33,11 +39,11 @@ public class SkuTranslator {
   }
 
   /**
-   *  Using given translation table, returns SKU unit 
-   * @param colour
-   * @param model
-   * @param isFront
-   * @return
+   *  Using given translation table, returns SKU unit given the info.
+   * @param colour the colour of this sku
+   * @param model the model of this sku
+   * @param isFront whether or not the sku is on the front or the back
+   * @return : the sku with the three above traits as an int
    */
   public static int getSku(String colour, String model, boolean isFront) {
     for (ArrayList<String> x : properties) {
@@ -53,6 +59,11 @@ public class SkuTranslator {
     return  -1;
   }
 
+  /**
+   *  Using given traversal table, returns location of <sku></sku> in warehouse.
+   * @param sku whether or not the sku is on the front or the back
+   * @return : the <sku></sku> with the given location as an int
+   */
   public static String getLocation(int sku) {
     String output = "None";
     for (ArrayList<String> x : locations) {
@@ -64,6 +75,13 @@ public class SkuTranslator {
     return output;
   }
 
+
+
+  /**
+   *  Using traversal table, fetches sku stored in <location></location>
+   * @param location is in String array format {Zone, Aisle, Rack, Rack Level}
+   * @return sku stored in given area according to translation table
+   */
   public static int getSkuFromLocation(String[] location) {
     for (ArrayList<String> x: locations) {
       if (String.join(",", location).toLowerCase().equals(
@@ -74,13 +92,20 @@ public class SkuTranslator {
     return -1;
   }
 
+  /**
+   *  Sets the traversal table to the csv file located in <path></path>
+   * @param path must be a valid path to a csv file
+   */
   public static void setLocations(String path)  {
     locations = CsvReadWrite.readAsArrays(path);
 
 
   }
 
-
+  /**
+   *  Sets the translation table to the csv file located in <path></path>
+   * @param path must be a valid path to a csv file
+   */
   public static void setProperties(String path) {
     properties = CsvReadWrite.readAsArrays(path);
   }
