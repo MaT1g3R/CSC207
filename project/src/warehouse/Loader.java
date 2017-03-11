c1521f176f245b6286ee77fcc30bca6217fd1b8d
betaman3 <chaitanya1998@gmail.com>
2017-03-10 23:54:03 -0500

    Properly formatted these three classes.

--- project/src/warehouse/Loader.java
+++ project/src/warehouse/Loader.java
@@ -1,4 +1,5 @@
 package warehouse;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -11,24 +12,25 @@ public class Loader extends Worker {
 
   private int[] frontPallet = new int[4];
   private int[] backPallet = new int[4];
-//  private static ArrayList<Truck> trucks; //MOVED TO WAREHOUSE
-  
-  public Loader(String name, Warehouse worksAt){
+  // private static ArrayList<Truck> trucks; //MOVED TO WAREHOUSE
+
+  public Loader(String name, Warehouse worksAt) {
     super(name, worksAt);
   }
-  
+
   public void load(int[] frontPallet, int[] backPallet) {
-//    int truckNum = trucks.size();
-//    if(truckNum == 0){ //Creates a new truck if none exist.
-//      trucks.add(new Truck());
-//      truckNum++;
-//    }
-//    else if(trucks.get(truckNum - 1).isFull()){
-//      
-//    }
+    // int truckNum = trucks.size();
+    // if(truckNum == 0){ //Creates a new truck if none exist.
+    // trucks.add(new Truck());
+    // truckNum++;
+    // }
+    // else if(trucks.get(truckNum - 1).isFull()){
+    //
+    // }
   }
+
   @Override
-  protected LinkedList<Integer> getScanOrder(){
+  protected LinkedList<Integer> getScanOrder() {
     return new LinkedList<>();
   }
 
--- project/src/warehouse/Truck.java
+++ project/src/warehouse/Truck.java
@@ -2,47 +2,47 @@ package warehouse;
 
 import java.util.ArrayList;
 
-public class Truck{
+public class Truck {
 
-  /*A list of 10 levels, with 4 pallets per level. Each pallet has 4 Facsia
+  /*
+   * A list of 10 levels, with 4 pallets per level. Each pallet has 4 Facsia
    * 
-   * the four pallets are represented like this in each level:
-   * FRONTLEFT BACKLEFT FRONTRIGHT BACKRIGHT
-   * */
+   * the four pallets are represented like this in each level: FRONTLEFT BACKLEFT FRONTRIGHT
+   * BACKRIGHT
+   */
   private ArrayList<ArrayList<int[]>> cargo;
-  
+
   private int currentLevel; // Current level that the pallets should be added to.
   private boolean addToLeft; // Decides whether to add to left side of truck or right.
-  
-  public Truck(){
+
+  public Truck() {
     cargo = new ArrayList<>();
-    cargo.add(new ArrayList<>()); //adding the first level to cargo
+    cargo.add(new ArrayList<>()); // adding the first level to cargo
     addToLeft = true;
     currentLevel = 0;
   }
-  
+
   public ArrayList<ArrayList<int[]>> getCargo() {
     return cargo;
   }
 
   public boolean isFull() {
-    if(cargo.size() != 10){
+    if (cargo.size() != 10) {
       return false;
-    }
-    else if(cargo.get(9).size() != 4){
+    } else if (cargo.get(9).size() != 4) {
       return false;
     }
     return true;
   }
 
   // loader gets 2 pallets(4 facsia each) from sequencer and adds it to truck.
-  public void addCargo(int[] frontPallet,int[] backPallet) {
-//    if(addToLeft){
-//      this.cargo.get(currentLevel).get
-//    } else{
-//      this.cargo.bad
-//      currentLevel++;
-//    }
-//    addToLeft = !addToLeft; //Next time add to the other side.
+  public void addCargo(int[] frontPallet, int[] backPallet) {
+    // if(addToLeft){
+    // this.cargo.get(currentLevel).get
+    // } else{
+    // this.cargo.bad
+    // currentLevel++;
+    // }
+    // addToLeft = !addToLeft; //Next time add to the other side.
   }
 }
