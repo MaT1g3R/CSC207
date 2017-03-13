d28c37ee14abfc7fb44c1093161680ce3bde188f
Unknown <982341777@qq.com>
2017-03-13 15:09:29 -0400

    Documented simulator

--- project/src/warehouse/Simulator.java
+++ project/src/warehouse/Simulator.java
@@ -38,7 +38,7 @@ class Simulator {
   }
 
   /**
-   * See if the string is for adding an order
+   * Check if the string is for adding an order
    *
    * @param s the string to be checked
    * @return true if it's adding an order
@@ -47,50 +47,123 @@ class Simulator {
     return Pattern.matches("Order \\w+ [A-Z]+", s);
   }
 
+  /**
+   * Check if the string is for ready a picker
+   *
+   * @param s the string to be checked
+   * @return true if it's ready a picker
+   */
   private boolean pickerReady(String s) {
     return Pattern.matches("Picker \\w+ ready", s);
   }
 
+  /**
+   * Check if the string is for picker pick
+   *
+   * @param s the string to be checked
+   * @return true if it's picker pick
+   */
   private boolean pickerPick(String s) {
     return Pattern.matches("Picker \\w+ pick [0-9]+", s);
   }
 
+  /**
+   * Check if the string is for picker to marshall
+   *
+   * @param s the string to be checked
+   * @return true if it's picker to marshall
+   */
   private boolean pickerMarshall(String s) {
     return Pattern.matches("Picker \\w+ to marshalling", s);
   }
 
+  /**
+   * Check if the string is for ready a sequencer
+   *
+   * @param s the string to be checked
+   * @return true if it's ready a sequencer
+   */
   private boolean sequencerReady(String s) {
     return Pattern.matches("Sequencer \\w+ ready", s);
   }
 
+  /**
+   * Check if the string is for sequencer scan
+   *
+   * @param s the string to be checked
+   * @return true if it's sequencer scan
+   */
   private boolean sequencerScan(String s) {
     return Pattern.matches("Sequencer \\w+ scan [0-9]+", s);
   }
 
+  /**
+   * Check if the string is for sequencer sequence
+   *
+   * @param s the string to be checked
+   * @return true if it's sequencer sequence
+   */
   private boolean sequencerSequence(String s) {
     return Pattern.matches("Sequencer \\w+ sequence", s);
   }
 
+  /**
+   * Check if the string is for ready a loader
+   *
+   * @param s the string to be checked
+   * @return true if it's ready a loader
+   */
   private boolean loaderReady(String s) {
     return Pattern.matches("Loader \\w+ ready", s);
   }
 
+  /**
+   * Check if the string is for loader scan
+   *
+   * @param s the string to be checked
+   * @return true if it's loader scan
+   */
   private boolean loaderScan(String s) {
     return Pattern.matches("Loader \\w+ scan [0-9]+", s);
   }
 
+  /**
+   * Check if the string is for loader load
+   *
+   * @param s the string to be checked
+   * @return true if it's loader load
+   */
   private boolean loaderLoad(String s) {
     return Pattern.matches("Loader \\w+ load", s);
   }
 
+  /**
+   * Check if the string is for replenish
+   *
+   * @param s the string to be checked
+   * @return true if it's replenish
+   */
   private boolean replenish(String s) {
     return Pattern.matches("Replenisher \\w+ replenish [0-9]+", s);
   }
 
+
+  /**
+   * Get the name of the worker from an event string
+   *
+   * @param s the event string
+   * @return the name of the worker
+   */
   private String getName(String s) {
     return s.split("\\s")[1];
   }
 
+  /**
+   * Get the sku number from an event string
+   *
+   * @param s the event string
+   * @return the sku number
+   */
   private int getSku(String s) {
     return Integer.valueOf(s.split("\\s")[3]);
   }
