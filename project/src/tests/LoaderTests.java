9f330e5a23c198484c53dfa6001a6f3de9c3c67c
betaman3 <chaitanya1998@gmail.com>
2017-03-14 23:59:12 -0400

    Added asserts to verify void methods.

--- project/src/tests/LoaderTests.java
+++ project/src/tests/LoaderTests.java
@@ -1,6 +1,9 @@
 package tests;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,8 +28,16 @@ public class LoaderTests {
   int[] frontPal = {1, 3, 5, 7};
   int[] backPal = {2, 4, 6, 8};
 
+  private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+
   @Before
   public void setUp() throws Exception {
+
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+
     SkuTranslator.setLocations("tests/translation.csv");
     SkuTranslator.setProperties("tests/traversal_table.csv");
     warehouse = new Warehouse("tests/initial.csv", "../", 30);
@@ -43,11 +54,15 @@ public class LoaderTests {
   }
 
   @After
-  public void tearDown() throws Exception {}
+  public void tearDown() throws Exception {
+
+  }
 
   @Test
   public void testReady() {
+    resetOutput();
     loader.ready();
+    assertEquals("Loader Joe is ready to load.\r\n", outContent.toString());
   }
 
   @Test
@@ -59,20 +74,27 @@ public class LoaderTests {
 
   @Test
   public void testBadLoad() {
+    resetOutput();
     loader.load();
+    assertEquals("The loader tried to load an incomplete picking request, "
+        + "the picking request was sent to be re picked instead.\r\n", outContent.toString());
   }
 
   @Test
   public void testGoodLoad() {
     loader.ready();
+    resetOutput();
     for (int i = 0; i < 8; i++) {
       loader.addScanCount();
     }
     loader.load();
+    assertEquals("Loader Joe could not load picking request 1\n"
+        + "The picking request is sent back to loading area.\r\n", outContent.toString());
   }
 
   @Test
   public void testGoodLoad1() {
+    resetOutput();
     pick = new PickingRequest(orders, 0);
     warehouse.sendToLoading(pick, frontPal, backPal);
     loader.ready();
@@ -80,5 +102,14 @@ public class LoaderTests {
       loader.addScanCount();
     }
     loader.load();
+    assertEquals("Loader Joe is ready to load.\r\n" + "Loader Joe loaded picking request 0\r\n",
+        outContent.toString());
+  }
+
+  public void resetOutput() {
+    outContent = new ByteArrayOutputStream();
+    errContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
   }
 }
