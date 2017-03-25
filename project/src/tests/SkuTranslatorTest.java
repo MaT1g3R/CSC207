f6260412c0a6ca007466490d820400b7aa03e8de
Unknown <982341777@qq.com>
2017-03-24 23:24:51 -0400

    SkuTranslatorTest done

--- project/src/tests/SkuTranslatorTest.java
+++ project/src/tests/SkuTranslatorTest.java
@@ -1,13 +1,27 @@
 package tests;
 
+import java.util.ArrayList;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import util.SkuTranslator;
 
 /**
  * Unit test for SkuTranslator class.
  */
 public class SkuTranslatorTest {
+
+  private static SkuTranslator translator;
+
+  /**
+   * Setup a SkuTranslator instance for all the tests to use.
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() {
+    translator = new TestFactory().getTestEnviroment().getSkuTranslator();
+  }
 
   @Before
   public void setUp() throws Exception {
@@ -19,24 +33,73 @@ public class SkuTranslatorTest {
 
   }
 
+  /**
+   * Test for getSku when it's for the front sku.
+   */
   @Test
-  public void getSku() throws Exception {
-
+  public void getSkuFront() {
+    Assert.assertEquals("9", translator.getSku("Beige", "S", true));
   }
 
+  /**
+   * Test for getSku when it's for the back sku.
+   */
   @Test
-  public void getLocation() throws Exception {
-
+  public void getSkuBack() {
+    Assert.assertEquals("24", translator.getSku("Red", "SEL", false));
   }
 
+  /**
+   * Test for getSku when the sku isn't in translation table.
+   */
   @Test
-  public void getSkuFromLocation() throws Exception {
-
+  public void getSkuFail() {
+    Assert.assertNull(translator.getSku("lol", "xd", true));
   }
 
+  /**
+   * Test for get location success.
+   */
   @Test
-  public void getAllSku() throws Exception {
-
+  public void getLocationSuccess() {
+    Assert.assertEquals("A,0,2,1", translator.getLocation("10"));
   }
 
+  /**
+   * Test for get location fail.
+   */
+  @Test
+  public void getLocationFail() {
+    Assert.assertNull(translator.getLocation("asdsdasad"));
+  }
+
+  /**
+   * Test for getSkuFromLocation success.
+   */
+  @Test
+  public void getSkuFromLocationSuccess() {
+    Assert.assertEquals("38",
+        translator.getSkuFromLocation(new String[]{"B", "1", "0", "1"}));
+  }
+
+  /**
+   * Test for getSkuFromLocation fail.
+   */
+  @Test
+  public void getSkuFromLocationFail() {
+    Assert.assertNull(
+        translator.getSkuFromLocation(new String[]{"asdasd", "NANI"}));
+  }
+
+  /**
+   * Test for getAllSku method.
+   */
+  @Test
+  public void getAllSku() {
+    ArrayList<String> expected = new ArrayList<>();
+    for (int i = 1; i < 49; i++) {
+      expected.add(String.valueOf(i));
+    }
+    Assert.assertEquals(expected, translator.getAllSku());
+  }
 }
